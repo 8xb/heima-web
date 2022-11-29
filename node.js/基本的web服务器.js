5fb68664666288b3b2e9e02a487f08ab181233dc
8xb <850110511@qq.com>
2022-11-29 17:13:54 +0800

    new file:   .gitignore new file:   node.js/package-lock.json new file:   node.js/package.json modified:   "node.js/\345\237\272\346\234\254\347\232\204web\346\234\215\345\212\241\345\231\250.js"

--- node.js/基本的web服务器.js
+++ node.js/基本的web服务器.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const moment = require('moment');
 const server = http.createServer();
 server.on('request', (req, res) => {
     res.setHeader('content-type', 'text/html;charset=utf-8');
-    res.end('<h1>世界你好！</h1>');
+    res.end(`<h1>世界你好！现在时间${moment().format('YYYY年MM月DD日 HH:mm:ss')}</h1>`);
     console.log('content!');
 });
 server.listen('80', () => {
