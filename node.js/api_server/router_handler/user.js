f03ca951e5f98c22abd9dfd9564563e308efc7e0
8xb <850110511@qq.com>
2022-12-28 10:51:00 +0800

    modified:   .gitignore modified:   node.js/api_server/package-lock.json modified:   node.js/api_server/package.json modified:   node.js/api_server/router/user.js modified:   node.js/api_server/router_handler/user.js new file:   node.js/api_server/schema/user.js

--- node.js/api_server/router_handler/user.js
+++ node.js/api_server/router_handler/user.js
@@ -2,6 +2,8 @@
 const db = require('../db/mysql');
 //导入bcryptjs加密包
 const bcryptjs = require('bcryptjs');
+//导入moment日期处理类库
+const moment = require('moment');
 
 //注册
 module.exports.regUser = (req, res) => {
@@ -10,54 +12,46 @@ module.exports.regUser = (req, res) => {
     const userinfo = req.body;
 
     //判断用户名或密码是否为空
-    if (!userinfo.username || !userinfo.password) {
-        return res.send({
-            status: 1,
-            msg: '用户名或密码不合法'
-        });
-    }
+    // if (!userinfo.user_name || !userinfo.user_password) {
+    //     return res.cc('用户名或密码不合法');
+    // }
 
     //定义sql语句，查询数据库是否占用
-    const sqlStr = 'select * from ev_users where username = ?';
-    db.query(sqlStr, [userinfo.username], (err, results) => {
+    const sqlStr = 'select * from ev_users where user_name = ?';
+    db.query(sqlStr, [userinfo.user_name], (err, results) => {
         //执行sql语句失败
         if (err) {
-            return res.send({
-                status: 1,
-                msg: err.message
-            });
+            return res.cc(err.message);
         }
 
         //判断用户是否被占用
         if (results.length > 0) {
-            return res.send({
-                status: 1,
-                msg: '用户名被占用，请更换其他用户名'
-            });
+            return res.cc('用户名被占用，请更换其他用户名');
         }
 
         //bcryptjs加密用户密码
-        userinfo.password = bcryptjs.hashSync(userinfo.password, 10);
+        userinfo.user_password = bcryptjs.hashSync(userinfo.user_password, 10);
 
         //定义sql语句，插入用户名和密码到数据库
         const sql = 'insert into ev_users set ?';
-        db.query(sql, { username: userinfo.username, password: userinfo.password }, (err, results) => {
+        const sqlQuery = {
+            user_name: userinfo.user_name,
+            user_password: userinfo.user_password,
+            //使用服务器端时间，非前端
+            user_registertime: moment().format('YYYY-MM-DD HH:mm:ss'),
+            user_nickname: userinfo.user_nickname,
+            user_email: userinfo.user_email,
+            user_pic: userinfo.user_pic
+        };
+        db.query(sql, sqlQuery, (err, results) => {
             //执行sql失败
-            if (err) return res.send({
-                status: 1,
-                msg: err.message
-            });
+            if (err) return res.cc(err.message);
             //sql执行成功，但影响行数不为1
             if (results.affectedRows !== 1) {
-                return res.send({
-                    status: 1,
-                    msg: '注册失败，稍后再试'
-                });
+                return res.cc('注册失败，稍后再试');
             }
-            res.send({
-                status: 0,
-                msg: '注册成功'
-            });
+            //sql执行成功，用户名和密码插入到数据库，注册成功
+            res.cc('注册成功');
         });
     });
 };
