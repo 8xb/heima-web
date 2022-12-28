2dd0f6526376cc807000b2974d47364a556d16f5
8xb <850110511@qq.com>
2022-12-28 10:50:24 +0800

    modified:   .gitignore modified:   node.js/api_server/package-lock.json modified:   node.js/api_server/package.json modified:   node.js/api_server/router/user.js modified:   node.js/api_server/router_handler/user.js
    node.js/api_server/schema/

--- node.js/api_server/app.js
+++ node.js/api_server/app.js
@@ -7,21 +7,41 @@ app.use(express.urlencoded({ extended: false }));
 
 //导入cors跨域模块
 const cors = require('cors');
+
 //注册全局cors
 app.use(cors());
+
+//注册res.cc函数,封装res.send()
+app.use((req, res, next) => {
+    // status = 0为成功；status = 1为失败；默认将status的值设置为1，方便处理失败的情况
+    res.cc = function (err, status) {
+        res.send({
+            // 状态
+            status: 1,
+            // 状态描述，判断err是错误对象还是字符串
+            message: err instanceof Error ? err.message : err
+        });
+    };
+    next();
+});
 
 //导入router路由模块
 const user_router = require('./router/user');
 //注册全局，并添加/api路径
 app.use('/api', user_router);
 
-//错误抛出
+//导入joi模块
+const joi = require('joi');
+//注册错误中间件 错误抛出
 app.use((err, req, res, next) => {
+    console.log('|||||||||||||||||||||出现一条错误 赶紧处理吧');
     console.error(err.stack);
-    res.status(500).send({
-        status: 500,
-        msg: '内部服务器错误!',
-    });
+    //数据验证失败
+    if (err instanceof joi.ValidationError) {
+        return res.cc(err);
+    }
+    //未知错误
+    res.cc(err);
 });
 
 
